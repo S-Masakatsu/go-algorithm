27b7e25efdbcdaeba0dc93dcd2ed7c4817f4d15f
S-Masakatsu <s.masakatsu197@gmail.com>
2022-02-11 18:00:26 +0900

    [update]random number

--- util/input.go
+++ util/input.go
@@ -5,11 +5,24 @@ import (
 	"time"
 )
 
-func RandSlice(size int) []int {
+type intRange struct {
+	min, max int
+}
+
+func newIntRange(min, max int) *intRange {
 	rand.Seed(time.Now().UnixNano())
+	return &intRange{min, max}
+}
+
+func (ir *intRange) Intn() int {
+	return rand.Intn(ir.max-ir.min+1) + ir.min
+}
+
+func RandSlice(size int) []int {
+	r := newIntRange(-100, 100)
 	ret := make([]int, size)
 	for i := 0; i < len(ret); i++ {
-		ret[i] = rand.Intn(100)
+		ret[i] = r.Intn()
 	}
 	return ret
 }
