cf213bb16df0c088a8c004652c511241f7482382
S-Masakatsu <s.masakatsu197@gmail.com>
2022-02-11 19:22:16 +0900

    [Add]random input search

--- util/input.go
+++ util/input.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"math/rand"
+	"sort"
 	"time"
 )
 
@@ -25,4 +26,20 @@ func RandSlice(size int) []int {
 		ret[i] = r.Intn()
 	}
 	return ret
+}
+
+func RandSearchSlice(size int, isSort bool) ([]int, int) {
+	nums := RandSlice(size)
+	if isSort {
+		sort.Ints(nums)
+	}
+
+	min, max := Min(nums...), Max(nums...)
+	search := rand.Intn(max-min+1) + min
+	// ↑だけだと存在しない値ばかりになるので共済処置
+	if i := rand.Intn(size + 10); i < len(nums) {
+		search = nums[i]
+	}
+
+	return nums, search
 }
